355f79f7dc245b323b5a53d012785947cecda74a
swarupSikder <swarupfx20@gmail.com>
2025-04-17 22:37:42 +0600

    route class added

---
+++ route.py
@@ -0,0 +1,8 @@
+class Route:
+    def __init__(self, start_point, end_point):
+        self.start_point = start_point
+        self.end_point = end_point
+
+    def __repr__(self):
+        return f'Route: {self.start_point} - {self.end_point}'
+    
