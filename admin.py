87d704f08cc5b35b508b42a041cf8f4b3d01a4f9
swarupSikder <swarupfx20@gmail.com>
2025-04-18 16:15:14 +0600

    invalid input handled

--- admin.py
+++ admin.py
@@ -11,10 +11,19 @@ class Admin:
     
     def add_bus(self, busSystem):
         print('\n<----[Add New Bus]---->')
-        bus_no = int(input('Bus Number : '))
-        bus_seat = int(input('Bus Seat : '))
-        start_point = input('Start point : ')
-        end_point = input('End point : ')
-        bus_1 = Bus(bus_no, Route(start_point, end_point) ,bus_seat)
+        bus_no = input('Bus Number: ')
+        bus_seat = input('Bus Seat: ')
+
+        try:
+            bus_no = int(bus_no)
+            bus_seat = int(bus_seat)
+        except ValueError:
+            print('Enter valid number input for Bus Number and Seat!')
+            return None
+
+        start_point = input('Start point: ')
+        end_point = input('End point: ')
+        
+        bus_1 = Bus(bus_no, Route(start_point, end_point), bus_seat)
         busSystem.add_bus(bus_1)
         print(f'\nSuccess! You have added a new Bus: Bus-[{bus_no}]')
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,99 @@
- passenger.book_ticket(bus, pa
+from passenger import Passenger
+from bus_system import BusSystem
+from admin import Admin
+from todo import Todo
+
+# Main DB
+busSystem = BusSystem()
+todoText = Todo()
+
+#   -   -   -   -   -   -   # 
+#                           #
+#           Admin           #
+#                           #
+#   -   -   -   -   -   -   #
+def admin_login():
+    print('\n<----[Admin Login]---->')
+    username = input('Admin Username: ')
+    password = input('Admin Password: ')
+
+    admin = Admin()
+    if username == admin.username and password == admin.get_password():
+        print(todoText.welcomeAdminText)
+
+        while True:
+            print(todoText.adminMenuText)
+            admin_opt = input('Admin Option: ')
+
+            if admin_opt == '1':
+                admin.add_bus(busSystem)
+            elif admin_opt == '2':
+                busSystem.show_bus_list()
+            elif admin_opt == '3':
+                print('<--------[Logged Out Successfully]-------->')
+                break
+            else:
+                print('Invalid Option! Try Again!')
+    else:
+        print('\n[x] Sorry! Something went wrong! Please try again!')
+
+
+
+
+
+
+
+#   -   -   -   -   -   -   # 
+#                           #
+#         Passenger         #
+#                           #
+#   -   -   -   -   -   -   #
+def book_ticket():
+    print(todoText.welcomePassengerText)
+    print('\n<----[Book Ticket]---->')
+
+    print('-> Please, provide rout info')
+    start_point = input('Current Location: ')
+    end_point = input('Destination: ')
+
+    bus = busSystem.find_bus(start_point, end_point)
+
+    if bus:
+        print(f'Bus [{bus.number}] is found on {bus.route}')
+        print(bus)
+        print('Now enter Passenger Info')
+        passenger_name = input('Passenger Name: ')
+        passenger_phone = input('Passenger Phone: ')
+        passenger = Passenger(passenger_name, passenger_phone, bus)
+        passenger.book_ticket(bus.number, passenger_name, passenger_phone)
+        busSystem.add_passenger(passenger)                              # not used, but saved for future work
+
+    else:
+        print('No bus is available for this route')
+
+
+
+
+
+
+
+#   -   -   -   -   -   -   # 
+#                           #
+#           Main            #
+#                           #
+#   -   -   -   -   -   -   #
+while True:
+    print(todoText.mainMenuText)
+    opt = input('Enter Option: ')
+    
+    if opt == '1':
+        admin_login()
+    elif opt == '2':
+        book_ticket()    # book ticket option
+    elif opt == '3':
+        busSystem.show_bus_list()
+    elif opt == '4':
+        print('<-------------[Exited Successfully]------------->')
+        break
+    else:
+        print('Invalid Option! Try Again!')
