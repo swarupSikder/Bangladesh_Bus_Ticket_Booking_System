46190729da2aa1a7cd51aa56fe1dd0ae8481bc2b
swarupSikder <swarupfx20@gmail.com>
2025-04-28 06:53:07 +0600

    compile all files in one

--- main.py
+++ main.py
@@ -1,7 +1,290 @@
-from passenger import Passenger
-from bus_system import BusSystem
-from admin import Admin
-from todo import Todo
+#   -   -   -   -   -   -   # 
+#                           #
+#        Bus System         #
+#                           #
+#   -   -   -   -   -   -   #
+class BusSystem:
+    def __init__(self):
+        self.busList = []
+        self.passengerList = []
+
+    def add_passenger(self, passenger):
+        self.passengerList.append(passenger)
+
+    def add_bus(self, bus):
+        self.busList.append(bus)
+
+    def show_bus_list(self):
+        print('<<<--------------[All Buses]-------------->>>')
+
+        if len(self.busList) == 0:
+            print('[X] Empty Station')
+        else:
+            for bus in self.busList:
+                print(bus)
+
+    def find_bus(self, start_point, end_point):
+        for bus in self.busList:
+            if bus.route.start_point.lower() == start_point.lower() and bus.route.end_point.lower() == end_point.lower() and bus.available_seats() != 0:
+                return bus
+        return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#   -   -   -   -   -   -   # 
+#                           #
+#         Bus Class         #
+#                           #
+#   -   -   -   -   -   -   #
+class Bus:
+    def __init__(self, number, route, total_seats):
+        self.number = number
+        self.route = route
+        self.total_seats = total_seats
+        self.booked_seats = 0
+        self.bookings = []
+
+    def available_seats(self):
+        return self.total_seats - self.booked_seats
+    
+    def book_seat(self, booking):
+        if self.total_seats - self.booked_seats == 0:
+            print(f'\n[x] Sorry! Booking is closed for this Bus with number : {self.number}')
+        else:
+            self.booked_seats += 1
+            self.bookings.append(booking)
+
+    def __repr__(self):
+        print('\n-------------------------------------------')
+        print(f'|               Bus [{self.number}]                |')
+        print('-------------------------------------------')
+        print(f'-> Route           |\t[{self.route.start_point} - {self.route.end_point}]')
+        print('-------------------------------------------')
+        print(f'-> Fare            |\t500 Taka (BDT)')
+        print('-------------------------------------------')
+        print(f'-> Total Seat      |\t{self.total_seats}')
+        print('-------------------------------------------')
+        print(f'-> Available seats |\t{self.available_seats()}')
+        print('-------------------------------------------')
+        print(f'-> Booked seats    |\t{self.booked_seats}')
+        print('-------------------------------------------')
+        print(f'|              Booking List               |')
+        print('|                                         |')
+        if not self.bookings:
+            print('|                  [X]                    |')
+            print('|                                         |')
+            print('-------------------------------------------')
+        else:
+            for booking in self.bookings:
+                print(booking)
+            print('|                                         |')
+            print('-------------------------------------------')
+        return ''
+
+
+
+
+
+
+
+
+
+
+
+
+
+#   -   -   -   -   -   -   # 
+#                           #
+#      Passenger Class      #
+#                           #
+#   -   -   -   -   -   -   #
+from datetime import datetime
+
+class Passenger:
+    def __init__(self, name, phone, bus):
+        self.name = name
+        self.phone = phone
+        self.bus = bus
+    
+    def book_ticket(self, bus_number, name, phone):
+        new_booking = Booking(bus_number, name, phone)
+        self.bus.book_seat(new_booking)
+        print(f'Congratulations! You have successfully booked a seat for Bus [{bus_number}] at {datetime.now()}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#   -   -   -   -   -   -   # 
+#                           #
+#        Route Class        #
+#                           #
+#   -   -   -   -   -   -   #
+class Route:
+    def __init__(self, start_point, end_point):
+        self.start_point = start_point
+        self.end_point = end_point
+
+    def __repr__(self):
+        return f'Route: [{self.start_point} - {self.end_point}]'
+
+
+
+
+
+
+
+
+
+
+
+
+#   -   -   -   -   -   -   # 
+#                           #
+#        Todo Class         #
+#                           #
+#   -   -   -   -   -   -   #
+class Todo:
+    def __init__(self):
+        self.mainMenuText = """
+----------<<BusCounterBD.com>>-----------
+1. Admin Login 
+2. Book Ticket 
+3. View Buses 
+4. Exit
+
+"""
+
+        self.adminMenuText = """
+<----[Admin Menu]---->
+1. Add Bus 
+2. View All Buses 
+3. Logout
+
+"""
+
+        self.welcomeAdminText = """
+---------------------------------------
+|   Welcome Admin! Login Successful   |
+---------------------------------------
+"""
+
+        self.welcomePassengerText = """
+-------------------------------------------
+|   Welcome Passenger! Login Successful   |
+-------------------------------------------
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#   -   -   -   -   -   -   # 
+#                           #
+#       Booking Class       #
+#                           #
+#   -   -   -   -   -   -   #
+class Booking:
+    def __init__(self, bus_number, name, phone):
+        self.bus_number = bus_number
+        self.name = name
+        self.phone = phone
+
+    def __repr__(self):
+        return f"|  <> Passenger: {self.name}, Phone: {self.phone}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#   -   -   -   -   -   -   # 
+#                           #
+#        Admin Class        #
+#                           #
+#   -   -   -   -   -   -   #
+class Admin:
+    def __init__(self):
+        self.username = 'admin'
+        self.__password = '1234'
+
+    def get_password(self):
+        return self.__password
+    
+    def add_bus(self, busSystem):
+        print('\n<----[Add New Bus]---->')
+        bus_no = input('Bus Number: ')
+        bus_seat = input('Bus Seat: ')
+
+        try:
+            bus_no = int(bus_no)
+            bus_seat = int(bus_seat)
+        except ValueError:
+            print('Enter valid number input for Bus Number and Seat!')
+            return None
+
+        start_point = input('Start point: ')
+        end_point = input('End point: ')
+        
+        bus_1 = Bus(bus_no, Route(start_point, end_point), bus_seat)
+        busSystem.add_bus(bus_1)
+        print(f'\nSuccess! You have added a new Bus: Bus-[{bus_no}]')
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Main DB
 busSystem = BusSystem()
